2c5604ecf6b76ad23dcd8b8cb13ca0109e8581f1
DanKendi <dan.kendisa@gmail.com>
2026-05-21 20:42:36 -0300

    Finalizando Sprint 4: - Preparando para deploy

--- src/main/java/br/com/easyaccess/easyaccess/config/FirebaseConfig.java
+++ src/main/java/br/com/easyaccess/easyaccess/config/FirebaseConfig.java
@@ -4,21 +4,28 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CREDENTIALS}")
+    private String firebaseCredentials;
+
     @PostConstruct
     public void initializeFirebase() throws IOException{
         if (FirebaseApp.getApps().isEmpty()){
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/easyaccess-firebase-service-account.json");
+            InputStream credentialsStream = new ByteArrayInputStream(
+                    firebaseCredentials.getBytes(StandardCharsets.UTF_8)
+            );
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(credentialsStream))
                     .build();
             FirebaseApp.initializeApp(options);
         }
